617f9bbc3ef87ad73209f309ed0c9acfbb377dc0
unknownSheep <65956553+unknownSheep@users.noreply.github.com>
2022-05-13 11:08:39 +0200

    Add files via upload
    final ino jean

--- is_jean_low.ino
+++ is_jean_low.ino
@@ -9,14 +9,24 @@
 #include <ArduinoJson.h>
 #include <WiFiUDP.h>
 #include <NTPClient.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 ESP8266WiFiMulti WiFiMulti;
 WiFiUDP ntpUDP;
-
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+#include <Servo.h>                    // include the Servo library
+Servo servo;  
+
 
 void setup() {
+  servo.attach(D5);
   Serial.begin(115200);
   // Serial.setDebugOutput(true);
 
@@ -33,41 +43,109 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP("redmi", "motdepasse");
 //  WiFiMulti.addAP("freebox_IYHYJI", "f7peterselie+");
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  display.clearDisplay();
 }
 
 void loop() {
   // wait for WiFi connection
   if ((WiFiMulti.run() == WL_CONNECTED))
   {
-//    String rep = doRequest("https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/nd-Hs3NUtQxSujekbANIOFXjE3IJ3bccHQPc668QYj5D0cwR41oN8bQikQ?api_key=RGAPI-1883218c-7ebf-4802-b262-b93d11983a7f");
-    String rep = doRequest("https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/7nqMfbimsivZUoTIv_At9yoSzBDUEVhRByKhErgUaKUXiL8?api_key=RGAPI-5483d786-8476-4579-b6d3-89eab98ed17e");
+//    String rep = doRequest("https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/nd-Hs3NUtQxSujekbANIOFXjE3IJ3bccHQPc668QYj5D0cwR41oN8bQikQ?api_key=api_key=RGAPI-41211a31-19f0-4274-942d-0cb5ec733d83");
+    String rep = doRequest("https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/3iA3RUBaWuMJVgCNsIBz63z02M9yR3e3z9gZHwcosNA9GXE?api_key=RGAPI-41211a31-19f0-4274-942d-0cb5ec733d83");
 //    String rep = doRequest("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/TranMinhVIE?api_key=RGAPI-3cc904fb-cae7-4e7c-b09f-0c337b58d663");
     Serial.println(rep);
 
     if(rep == "ERROR") // ERROR =>
     {
-
+      servo.write(0);
+      
+      display.clearDisplay();
+      display.setTextColor(WHITE);
+      
+      display.setTextSize(3);
+      display.setCursor(0,0);
+      display.print("ERROR");
+      display.display();
     }
     else if (rep == "OFF") // OFFLINE =>
     {
+      servo.write(0);
+      display.clearDisplay();
+      display.setTextColor(WHITE);
       
+      display.setTextSize(7);
+      display.setCursor(0,0);
+      display.print("OFF");
+      display.display();
     }
     else // ONLINE
     {
+      servo.write(90);    
       DynamicJsonBuffer jsonBuffer;
 
       JsonObject& root = jsonBuffer.parseObject(rep);
 
       if (!root.success()) {
         Serial.println("parseObject() failed");
-        delay(10000);
         return;
       }
       long long start_time = root["gameStartTime"];
       jsonBuffer.clear();
-      Serial.println(getTime() - (start_time/1000));
+      long long duration = getTime() - (start_time/1000);
+
+      int minutes = floor(duration/60) - 1;
+      int seconds = duration % 60;
+      
+      Serial.println(minutes);
+      Serial.println(seconds);
+      
+      display.clearDisplay();
+      display.setTextColor(WHITE);
+      
+      display.setTextSize(2);
+      display.setCursor(0,0);
+      display.print("In game :");
+      
+//      display.setTextSize(3);
+//      display.setCursor(32, 32);
+//      display.print(duration);
+      display.setTextSize(3);
+      
+      display.setTextSize(3);
+      
+      String output;
+      if (minutes >= 10)
+      {
+        output.concat(String(minutes));
+      }
+      else
+      {
+        output.concat("0");
+        output.concat(String(minutes));
+      }
+      output.concat(":");
+      
+      if (seconds >= 10)
+      {
+        output.concat(String(seconds));
+      }
+      else
+      {
+        output.concat("0");
+        output.concat(String(seconds));
+      }
+      
+      display.setCursor(15, 32);
+      display.print(output);
+  
+      display.display();
     }
-//    delay(1000);
   }
 }
 
